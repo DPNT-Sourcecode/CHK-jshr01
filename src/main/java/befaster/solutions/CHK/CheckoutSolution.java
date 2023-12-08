017959999b6829920700c63b73377e85f2d5a574
sbx_user1051 <sbx_user1051@169.254.14.221>
2023-12-08 12:16:18 +0000

    Fri Dec 08 12:16:18 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,14 +8,45 @@ import java.util.Map;
 public class CheckoutSolution {
     private final Map<String, Integer> pricesMap = new HashMap<>();
 
-
     public Integer checkout(String skus) {
+        if(skus == null || skus.isEmpty()){
+            return -1;
+        }
+
+        //item / quantity
         Map<String, Integer> shoppingCartMap = new HashMap<>();
 
-        
+        populatePricesMap();
+
+        if(!parsedSkus(skus, shoppingCartMap)){
+            return -1;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Parse sku string
+     * @param skus, string with skus to parse
+     * @param shoppingCartMap, map with items on cart
+     * @return true is string is totally parsed, and false if there's any invalid sku code in it
+     */
+    private boolean parsedSkus(String skus, Map<String, Integer> shoppingCartMap){
+        for(int i = 0; i < skus.length(); i++){
+            if(pricesMap.get(skus.charAt(i)+"") != null){
+                pricesMap.get(skus.charAt(i)+"");
+            }else{
+                return false;
+            }
+        }
+
+        return true;
     }
 
 
+    /**
+     * initialize with item prices
+     */
     private void populatePricesMap(){
         pricesMap.put("A", 50);
         pricesMap.put("B", 30);
@@ -23,5 +54,6 @@ public class CheckoutSolution {
         pricesMap.put("D", 15);
     }
 }
+
 
 
